5997b77afb6ef3ed27a30374c76e1ba70191369e
Vivica <Ma>
2020-11-02 23:17:34 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Owner.delete_all
+Item.delete_all
+Category.delete_all
+Image.delete_all
+Market.delete_all
+
